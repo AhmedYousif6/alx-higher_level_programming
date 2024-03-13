b29c809d51b8159214422a43d934ef5f33ef7ad0
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-03-13 15:46:13 -0700

    new files

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- create table (first_table) if not exist
+CREATE TABLE first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- display the full description of first_table
+-- the database name will be passed as an argument
+SHOW CREATE TABLE first_table;
